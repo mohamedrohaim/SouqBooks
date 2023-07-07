26a17b36f6203cff3f48f4915c57edd795723e20
mohamedRohiem <mohamedrohiem597@gmail.com>
2023-07-07 11:42:54 +0200

    Edit,Delete Category

--- SouqBooks/Controllers/CategoryController.cs
+++ SouqBooks/Controllers/CategoryController.cs
@@ -35,5 +35,71 @@ namespace SouqBooks.Controllers
             return View(category);
             }
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id != null) {
+                var category= _context.categories.FirstOrDefault(c => c.Id == id);
+                if (category != null)
+                {
+                    return View(category);
+                }
+                else { 
+                return NotFound();
+                }
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid )
+            {
+                _context.categories.Update(category);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            else { 
+            return View(category);
+            }
+        }
+
+
+        public IActionResult Delete(int id)
+        {
+            if (id != null)
+            {
+                var category = _context.categories.FirstOrDefault(c => c.Id == id);
+                if (category != null)
+                {
+                    return View(category);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            return NotFound();
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.categories.Remove(category);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(category);
+            }
+        }
+
     }
 }
